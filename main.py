df1fc40aed8208cd29fbed3390d5deab6571b85f
shashank-kodali <k.shashankvinayak@gmail.com>
2025-03-25 21:32:44 +0530

    Save Password
    passwords can be saved by using the add  button and the data is saved in a txt file .

--- main.py
+++ main.py
@@ -2,7 +2,19 @@ from tkinter import *
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
 
 # ---------------------------- SAVE PASSWORD ------------------------------- #
+def savePassword():
+    website = website_input.get()
+    email = email_input.get()
+    password = password_input.get()
 
+    website_input.delete(0,"end")
+    email_input.delete(0,"end")
+    password_input.delete(0,"end")
+
+    website_input.focus()
+
+    with open("mypassword.txt",mode="a") as data:
+        data.write(f"{website} || {email} || {password}\n")
 # ---------------------------- UI SETUP ------------------------------- #
 window = Tk()
 window.title("PassWord Manager")
@@ -23,6 +35,7 @@ password_label = Label(text="Password :")
 password_label.grid(column=0,row=3)
 
 website_input = Entry(width=35)
+website_input.focus()
 website_input.grid(column=1, row=1, columnspan=2)
 
 email_input = Entry(width=35)
@@ -34,7 +47,7 @@ password_input.grid(column=1,row=3, columnspan=2)
 generate_button = Button(text="Generate Password",width=30)
 generate_button.grid(column=1,row=4,columnspan=2)
 
-add_button = Button(text="Add",width=30)
+add_button = Button(text="Add",width=30,command=savePassword)
 add_button.grid(column=1,row=5,columnspan=2)
 
 
