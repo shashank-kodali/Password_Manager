253043684a65ecd50be9839de02fa648b7ad7158
shashank-kodali <k.shashankvinayak@gmail.com>
2025-03-25 22:33:30 +0530

    Completed
    In this final commit I added a password generator to the program.

--- main.py
+++ main.py
@@ -1,5 +1,21 @@
 from tkinter import *
+from tkinter import messagebox
+from random import choice,randint,shuffle
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
+def generatorPassword():
+    letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+    numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+    symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
+
+    password_letters = [choice(letters) for _ in range(randint(8, 10))]
+    password_numbers = [choice(numbers) for _ in range(randint(2, 4))]
+    password_symbols = [choice(symbols) for _ in range(randint(2, 4))]
+
+    password_list = password_letters + password_numbers + password_symbols
+    shuffle(password_list)
+    password = "".join(password_list)
+
+    password_input.insert(0,string=password)
 
 # ---------------------------- SAVE PASSWORD ------------------------------- #
 def savePassword():
@@ -7,14 +23,25 @@ def savePassword():
     email = email_input.get()
     password = password_input.get()
 
-    website_input.delete(0,"end")
-    email_input.delete(0,"end")
-    password_input.delete(0,"end")
+    
+    if len(website)==0 or len(email)==0 or len(password)==0 :
+        messagebox.showinfo(title="Oops",message="Don't leave any fields empty.")
 
-    website_input.focus()
+    else:
+        is_ok = messagebox.askokcancel(title=website,message=f"""This are the details entered :
+                                   \n Email : {email} \n Password : {password} """)
+        
+        if is_ok:
+            with open("mypassword.txt",mode="a") as data:
+                data.write(f"{website} || {email} || {password}\n")
+                website_input.delete(0,"end")
+                email_input.delete(0,"end")
+                password_input.delete(0,"end")
+                website_input.focus()
 
-    with open("mypassword.txt",mode="a") as data:
-        data.write(f"{website} || {email} || {password}\n")
+    
+
+    
 # ---------------------------- UI SETUP ------------------------------- #
 window = Tk()
 window.title("PassWord Manager")
@@ -44,7 +71,7 @@ email_input.grid(column=1,row=2, columnspan=2)
 password_input = Entry(width=35)
 password_input.grid(column=1,row=3, columnspan=2)
 
-generate_button = Button(text="Generate Password",width=30)
+generate_button = Button(text="Generate Password",width=30, command=generatorPassword)
 generate_button.grid(column=1,row=4,columnspan=2)
 
 add_button = Button(text="Add",width=30,command=savePassword)
