d9e2f03ca5486371e5f5f699878d53e926cbddbb
shashank-kodali <k.shashankvinayak@gmail.com>
2025-03-25 18:44:37 +0530

    Added Widgets
    Added all the widgets required for project

--- main.py
+++ main.py
@@ -6,11 +6,36 @@ from tkinter import *
 # ---------------------------- UI SETUP ------------------------------- #
 window = Tk()
 window.title("PassWord Manager")
-window.config(padx=20,pady=20)
+window.config(padx=50,pady=50)
 
 canvas = Canvas(width=200,height=200)
 logo_img = PhotoImage(file="logo.png")
 canvas.create_image(100,100,image=logo_img)
-canvas.pack()
+canvas.grid(column=1,row=0)
+
+website_label = Label(text="Website :")
+website_label.grid(column=0,row=1)
+
+email_label = Label(text="Email/Username :")
+email_label.grid(column=0,row=2)
+
+password_label = Label(text="Password :")
+password_label.grid(column=0,row=3)
+
+website_input = Entry(width=35)
+website_input.grid(column=1, row=1, columnspan=2)
+
+email_input = Entry(width=35)
+email_input.grid(column=1,row=2, columnspan=2)
+
+password_input = Entry(width=35)
+password_input.grid(column=1,row=3, columnspan=2)
+
+generate_button = Button(text="Generate Password",width=30)
+generate_button.grid(column=1,row=4,columnspan=2)
+
+add_button = Button(text="Add",width=30)
+add_button.grid(column=1,row=5,columnspan=2)
+
 
 window.mainloop()
---
+++ tempCodeRunnerFile.py
@@ -0,0 +1,8 @@
+website_input = Entry(width=35)
+website_input.grid(column=1, row=1, columnspan=2)
+
+email_input = Entry(width=35)
+email_input.grid(column=1,row=2, columnspan=2)
+
+password_input = Entry(width=20)
+password_input.grid(column=1,row=3,)
