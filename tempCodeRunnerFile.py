646ccc5e43d07b35a281ee10dd1605ac3eef5b11
shashank-kodali <k.shashankvinayak@gmail.com>
2025-03-25 21:37:39 +0530

    Update tempCodeRunnerFile.py

--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -1,8 +1,1 @@
-website_input = Entry(width=35)
-website_input.grid(column=1, row=1, columnspan=2)
 
-email_input = Entry(width=35)
-email_input.grid(column=1,row=2, columnspan=2)
-
-password_input = Entry(width=20)
-password_input.grid(column=1,row=3,)
